20068311c6200b640144099fc99d2493ac72262a
cdJohnEl <easykelchimdikejohn@gmail.com>
2024-02-09 16:19:13 +0100

    Frontend

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { FaBarsStaggered, FaXmark } from "react-icons/fa6";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const handleMenuToggler = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const navItems = [
+    { path: "/", title: "Home" },
+    { path: "/my-job", title: "My Jobs" },
+    // {path: '/', title: 'Salary Estimate'},
+    { path: "/post-job", title: "Upload Jobs" },
+  ];
+  return (
+    <header className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
+      <nav className="flex justify-between items-center py-6">
+      <a
+            class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
+            href="#"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-10 h-10 text-white p-2 bg-jobBlue rounded-full"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"
+              ></path>
+            </svg>
+            <span class="ml-3 text-xl">Job Portal</span>
+          </a>
+
+        {/* Large screens */}
+
+        <ul className="hidden md:flex gap-12 ">
+          {navItems.map(({ path, title }) => (
+            <li key={path} className="text-base text-primary">
+              <NavLink
+                to={path}
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                {title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+
+        {/* SignUP and log in buttons */}
+        <div className="text-base text-primary font-medium space-x-5 hidden lg:block">
+          <Link to="/login" className="py-2 px-5 border rounded">
+            Log in
+          </Link>
+          <Link
+            to="/sign-up"
+            className="py-2 px-5 border rounded bg-blue-800 text-white"
+          >
+            Sign up
+          </Link>
+        </div>
+
+        {/* Mobile screens */}
+        <div className="md:hidden block">
+          <button onClick={handleMenuToggler}>
+            {isMenuOpen ? (
+              <FaXmark className="w-5 h-5 text-primary" />
+            ) : (
+              <FaBarsStaggered className="w-5 h-5 text-primary" />
+            )}
+          </button>
+        </div>
+      </nav>
+
+      {/* Navitems for mobile screen */}
+      <div
+        className={`px-4 bg-black py-5 rounded-sm ${
+          isMenuOpen ? "" : "hidden"
+        }`}
+      >
+        <ul>
+          {navItems.map(({ path, title }) => (
+            <li
+              key={path}
+              className="text-base text-white first:text-white py-1"
+            >
+              <NavLink
+                to={path}
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                {title}
+              </NavLink>
+            </li>
+          ))}
+
+          <li className="text-white py-1">
+            <Link to="/login">Log in</Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
